8b253e14ac70ed0938c7efcd598941c365b3ec3b
Dif-scope <3954396214@qq.com>
2026-01-07 15:01:19 +0800

    Add exchange function to swap two integers
    Implement a function to exchange two integer values using pointers.

---
+++ F.6 practice.c
@@ -0,0 +1,23 @@
+//指针做参数，事项两个参数值的交换并返回结果
+
+#include<stdio.h>
+int a,b;
+int*p=&a,*q=&b;
+
+void exchange(int *ptr1,int *ptr2)
+{
+	int temp;
+	temp=*p;
+	*p=*q;
+	*q=temp;
+ } 
+
+int main()
+{
+	printf("input 2 num to exchange:");
+	scanf("%d%d",&a,&b);
+	printf("before exchange:%d %d",a,b);
+	exchange(p,q);
+	printf("echanged:%d %d",a,b);
+	return 0;
+}
